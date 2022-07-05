32a29390788e8ca69859fca18b51cf41685697be
Asım Tahir <a.karakus@pa.ag>
2022-07-05 10:47:52 +0300

    chore: remove visit example cypress website

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,8 +1,6 @@
 /// <reference types="cypress" />
 describe("page", () => {
   it("should convert i18n keys", () => {
-    cy.visit("https://example.cypress.io");
-
     cy.$t("hello").should("be.eq", "Hello");
     cy.$t("world").should("be.eq", "World");
 
