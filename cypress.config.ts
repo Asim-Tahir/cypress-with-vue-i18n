f28d99c2b876ae771f8333f5a4bc3d6aa5648986
Asım Tahir <a.karakus@pa.ag>
2022-07-18 17:38:47 +0300

    ci(e2e_tests): :green_heart: dns workarounds test in docker

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {},
-    baseUrl: "http://localhost:4173",
+    baseUrl: "http://thissiteisunexist.com:4173",
     specPattern: "cypress/e2e/**/*.{test,cy}.{js,ts}",
   },
 });
